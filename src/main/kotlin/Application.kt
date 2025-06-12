fa557eac9b773b77fe07b60165548c82c99cba7e
Hannes Morgenthaler <hanneszbinden@outlook.com>
2025-06-12 21:56:15 +0000

    add initial kotlin code + java extension.

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package ch.abb-ts
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
