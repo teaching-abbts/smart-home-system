d4a30f483da472fe5bb5a4d530cd44a94eba0fab
Hannes Morgenthaler <hanneszbinden@outlook.com>
2025-06-13 01:10:32 +0000

    remove strange html dsl, working www-form example.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ repositories { mavenCentral() }
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
-    implementation(libs.ktor.server.html)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,12 +1,13 @@
 package ch.abbts
 
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.netty.*
-import io.ktor.server.html.*
-import io.ktor.http.HttpStatusCode
-import kotlinx.html.*
+import io.ktor.http.*
+
+data class User(val name: String, val email: String)
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -14,30 +15,82 @@ fun main(args: Array<String>) {
 
 fun Application.myModule() {
     routing {
+        val sendActionPath = "/senden"
+
         get("/") {
             val title = "Mein erstes Formular"
 
-            call.respondHtml(HttpStatusCode.OK) {
-                head {
-                    title {
-                        +title
-                    }
-                }
-                body {
-                    h2 {
-                        +title
-                    }
-                    form(action = "/senden", method = FormMethod.post) {
-                        label("name") {
-                            +"Name:"
+            call.respondText(
+                """
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>${title}</title>
+                        <style>
+                        body {
+                            width: 100%;
                         }
-                        textInput(name="name") {}
-                        submitInput() {
-                            value = "Senden"
+
+                        h2 {
+                            text-align: center;
                         }
-                    }
-                }
-            }
+
+                        form {
+                            width: 300px;
+                            margin: 0 auto;
+                            padding: 20px;
+                            border-radius: 10px;
+                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                            background-color: white;
+                        }
+
+                        input[type="submit"] {
+                            margin-top: 5px;
+                        }
+                        </style>
+                    </head>
+                    <body>
+                        <h2>${title}</h2>
+                        <form action="${sendActionPath}" method="post">
+                        <label for="name">Name:</label><br />
+                        <input type="text" id="name" name="name" /><br />
+                        <label for="email">Email:</label><br />
+                        <input type="email" id="email" name="email" /><br />
+                        <input type="submit" value="Submit" />
+                        </form>
+                    </body>
+                    </html>
+                """,
+                ContentType.Text.Html,
+                HttpStatusCode.OK)
+        }
+        post(sendActionPath) {
+            // transmit data via POST
+            // Content-Type: application/x-www-form-urlencoded
+            val formParameters = call.receiveParameters()
+            val name = formParameters["name"].toString()
+            val email = formParameters["email"].toString()
+
+            val title = "Resultate"
+
+            call.respondText(
+                """
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>${title}</title>
+                    </head>
+                    <body>
+                        <h2>${title}</h2>
+                        <ul>
+                            <li>name: ${name}</li>
+                            <li>email: ${email}</li>
+                        </ul>
+                    </body>
+                    </html>
+                """,
+                ContentType.Text.Html,
+                HttpStatusCode.OK)
         }
     }
 }
