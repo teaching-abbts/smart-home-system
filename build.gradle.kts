bcf8756e3ad91fc3cbbdc9c6c05763d10a138563
Hannes Morgenthaler <hanneszbinden@outlook.com>
2025-06-13 00:40:36 +0000

    initial form response

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories { mavenCentral() }
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.html)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,6 +4,9 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.netty.*
+import io.ktor.server.html.*
+import io.ktor.http.HttpStatusCode
+import kotlinx.html.*
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -12,7 +15,29 @@ fun main(args: Array<String>) {
 fun Application.myModule() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val title = "Mein erstes Formular"
+
+            call.respondHtml(HttpStatusCode.OK) {
+                head {
+                    title {
+                        +title
+                    }
+                }
+                body {
+                    h2 {
+                        +title
+                    }
+                    form(action = "/senden", method = FormMethod.post) {
+                        label("name") {
+                            +"Name:"
+                        }
+                        textInput(name="name") {}
+                        submitInput() {
+                            value = "Senden"
+                        }
+                    }
+                }
+            }
         }
     }
 }
