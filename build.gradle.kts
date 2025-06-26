3c2453476fa88456873662f016a3916af3abafd2
Hannes Morgenthaler <hanneszbinden@outlook.com>
2025-06-26 20:02:57 +0200

    fix indenting.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.ktor)
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.kotlin.serialization)
+  alias(libs.plugins.ktor)
 }
 
 group = "abb-ts.ch"
@@ -13,13 +13,13 @@ application { mainClass = "io.ktor.server.netty.EngineMain" }
 repositories { mavenCentral() }
 
 dependencies {
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.config.yaml)
-    implementation(libs.ktor.serialization.json)
-    implementation(libs.ktor.server.content.negotiation)
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
+  implementation(libs.ktor.server.core)
+  implementation(libs.ktor.server.netty)
+  implementation(libs.logback.classic)
+  implementation(libs.ktor.server.core)
+  implementation(libs.ktor.server.config.yaml)
+  implementation(libs.ktor.serialization.json)
+  implementation(libs.ktor.server.content.negotiation)
+  testImplementation(libs.ktor.server.test.host)
+  testImplementation(libs.kotlin.test.junit)
 }
