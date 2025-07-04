b90a3153961c93f91a9565950733ea0216ab5750
Hannes Morgenthaler <10463536+mumrich@users.noreply.github.com>
2025-07-04 10:02:09 +0200

    install + setup vuetify

--- src/main/vue-project/src/App.vue
+++ src/main/vue-project/src/App.vue
@@ -1,86 +1,43 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/gallery">Gallery</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+      <v-divider />
+      <v-container>
+        <v-switch
+          v-model="isDarkTheme"
+          true-icon="mdi-moon-waning-crescent"
+          false-icon="mdi-white-balance-sunny"
+          density="comfortable"
+          hide-details
+        />
+      </v-container>
+      <v-divider />
+      <v-list-item link title="Home" to="/"></v-list-item>
+      <v-list-item link title="Gallery" to="/gallery"></v-list-item>
+      <v-list-item link title="About" to="/about"></v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <RouterView />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import { useTheme } from "vuetify";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const theme = useTheme();
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+const drawer = ref<boolean | null>(null);
+const isDarkTheme = computed({
+  get: () => theme.current.value.dark,
+  set: (v) => (theme.global.name.value = v ? "dark" : "light"),
+});
+</script>
--- src/main/vue-project/src/main.ts
+++ src/main/vue-project/src/main.ts
@@ -1,11 +1,11 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./plugins/router";
+import vuetify from "./plugins/vuetify";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
