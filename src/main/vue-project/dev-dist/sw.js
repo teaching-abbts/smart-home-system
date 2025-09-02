ce33e5626f61fc032561e47ccbfc3366ecb03650
Hannes Morgenthaler <10463536+mumrich@users.noreply.github.com>
2025-09-02 22:58:14 +0200

    Enable user info endpoint authentication and update development mode in configuration

--- src/main/vue-project/dev-dist/sw.js
+++ src/main/vue-project/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qngnjqa4klo"
+    "revision": "0.1dqmrc7gdr"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
